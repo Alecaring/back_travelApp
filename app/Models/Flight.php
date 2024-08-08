dc2f13da1ec635b03532e1241a6ee4551d55be98
DanieleBaggiano <danielebaggiano@gmail.com>
2024-08-08 17:09:25 +0200

    add populate flight table

--- app/Models/Flight.php
+++ app/Models/Flight.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Flight extends Model
 {
     use HasFactory;
+
+    protected $table = 'flights';
+
+    protected $fillable = [
+        'airlineName',
+        'sourceCity',
+        'destinationCity',
+        'departureTime',
+        'arrivalTime'
+    ];
 }
--- database/seeders/flightsSeeder.php
+++ database/seeders/flightsSeeder.php
@@ -2,16 +2,54 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
-class flightsSeeder extends Seeder
+class FlightsSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        //
+        $flights = [
+            [
+                'airlineName' => 'American Airlines',
+                'sourceCity' => 'New York',
+                'destinationCity' => 'Los Angeles',
+                'departureTime' => '2024-08-10 08:00:00',
+                'arrivalTime' => '2024-08-10 11:00:00',
+            ],
+            [
+                'airlineName' => 'British Airways',
+                'sourceCity' => 'London',
+                'destinationCity' => 'Paris',
+                'departureTime' => '2024-08-10 09:00:00',
+                'arrivalTime' => '2024-08-10 10:30:00',
+            ],
+            [
+                'airlineName' => 'Lufthansa',
+                'sourceCity' => 'Frankfurt',
+                'destinationCity' => 'Berlin',
+                'departureTime' => '2024-08-10 07:00:00',
+                'arrivalTime' => '2024-08-10 08:15:00',
+            ],
+            [
+                'airlineName' => 'Air France',
+                'sourceCity' => 'Paris',
+                'destinationCity' => 'New York',
+                'departureTime' => '2024-08-10 14:00:00',
+                'arrivalTime' => '2024-08-10 17:30:00',
+            ],
+            [
+                'airlineName' => 'Emirates',
+                'sourceCity' => 'Dubai',
+                'destinationCity' => 'Sydney',
+                'departureTime' => '2024-08-10 02:00:00',
+                'arrivalTime' => '2024-08-10 14:00:00',
+            ],
+        ];
+
+        DB::table('flights')->insert($flights);
     }
 }
