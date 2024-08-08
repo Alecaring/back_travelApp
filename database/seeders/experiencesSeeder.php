630f01116029c786840bc5dfcb6c3c703c6bb4cb
DanieleBaggiano <danielebaggiano@gmail.com>
2024-08-08 16:30:40 +0200

    create table

---
+++ database/seeders/experiencesSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class experiencesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
