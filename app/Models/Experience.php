c3cec5ddc5f83b79c927509da84510cdf42b94ab
DanieleBaggiano <danielebaggiano@gmail.com>
2024-08-08 17:16:25 +0200

    add populate experience table

--- app/Models/Experience.php
+++ app/Models/Experience.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Experience extends Model
 {
     use HasFactory;
+
+    protected $table = 'experiences';
+
+    protected $fillable = [
+        'name',
+        'location'
+    ];
 }
