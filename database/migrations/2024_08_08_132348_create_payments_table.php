0b285e69e39113ff665648efa5faa1808bf89b7a
Alecaring <Private_a.a.c@icloud.com>
2024-08-08 16:11:19 +0200

    add: hotel modal & table

--- database/migrations/2024_08_08_132348_create_payments_table.php
+++ database/migrations/2024_08_08_132348_create_payments_table.php
@@ -13,13 +13,12 @@ return new class extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id('PaymentID');
-            $table->unsignedBigInteger('BookingID'); // Cambiato da $table->id a $table->unsignedBigInteger
-            $table->decimal('Amount', 10, 2); // Cambiato per usare il parametro giusto
-            $table->dateTime('PaymentDate');
+            $table->unsignedBigInteger('BookingID')->nullable();
+            $table->decimal('Amount', 10, 2)->nullable();
+            $table->dateTime('PaymentDate')->nullable();
             $table->timestamps();
 
-            // Chiave esterna per BookingID
-            $table->foreign('BookingID')->references('BookingID')->on('bookings')->onDelete('cascade');
+            // $table->foreign('BookingID')->references('BookingID')->on('bookings')->onDelete('cascade');
         });
     }
 
