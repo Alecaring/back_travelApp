ed8fa7282aad579cb40165d90afce040659a65ef
Alecaring <Private_a.a.c@icloud.com>
2024-08-12 22:04:51 +0200

    add: CoverImage, imagesHotelId on hotels table

--- database/migrations/2024_08_08_134227_create_hotels_table.php
+++ database/migrations/2024_08_08_134227_create_hotels_table.php
@@ -18,7 +18,11 @@ return new class extends Migration
             $table->string('Address', 255)->nullable();
             $table->decimal('Latitude', 9, 6)->nullable();
             $table->decimal('Longitude', 9, 6)->nullable();
+            $table->string('CoverImage')->nullable();
+            $table->unsignedBigInteger('imagesHotelId')->nullable();
             $table->timestamps();
+
+            // $table->foreign('imagesHotelId')->references('imagesHotelId')->on('HotelsImages')->onDelete('cascade');
         });
     }
 
